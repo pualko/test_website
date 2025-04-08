c9efc1de908b9ba1244f87c192447939a46c2001
KF <kenfu658@gmail.com>
2025-04-08 19:10:53 +0800

    完成首頁

--- static/frontpage.js
+++ static/frontpage.js
@@ -1,3 +1,4 @@
+// 切換QA種類按鈕
 function showContent(id, event) {
     // 隱藏所有內容
     var contents = document.querySelectorAll(".content");
@@ -14,6 +15,7 @@ function showContent(id, event) {
     event.currentTarget.classList.add("active");
 }
 
+//step按鈕
 let currentStep = 1; // 目前步驟 (從 1 開始)
 const totalSteps = 4; // 總步驟數
 
@@ -46,3 +48,20 @@ nextBtn.addEventListener("click", () => {
 
 // 初始化 (確保 Step 1 顯示)
 updateSteps();
+
+// QA按鈕
+function toggleContent(button) {
+    var answerDiv = button.closest('.QA').querySelector('.answer');
+    var img = button.querySelector('img');  // 找到按鈕內的圖片
+    
+    // 切換顯示/隱藏的狀態
+    if (answerDiv.style.display === 'none' || answerDiv.style.display === '') {
+        answerDiv.style.display = 'block';  // 顯示更多文字
+        img.src = "../static/image/frontpage_image/首頁素材_E-icon-合起-一般.png";  // 更換圖片
+        button.classList.add('open');
+    } else {
+        answerDiv.style.display = 'none';  // 隱藏文字
+        img.src = "../static/image/frontpage_image/首頁素材_E-icon-展開-一般.png";
+        button.classList.remove('open');
+    }
+}
